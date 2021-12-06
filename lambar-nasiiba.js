2a264b9c7b01c089c2f6d9d6327d5b36e29131c4
a2uuz <aziznageeye9@gmail.com>
2021-12-06 11:52:29 +0300

    now input 2 numbers and make a random one in between

--- lambar-nasiiba.js
+++ lambar-nasiiba.js
@@ -1,13 +1,18 @@
 // Collect input from a user
-let sheegLambar = prompt(`walaal gali lambar`);
+let lambarYar = prompt(`gali lambar yar`);
+let lambarWayn = prompt(`gali lambar wayn`);
 
 // Convert the input to a number
-let wakanLambarku = +sheegLambar;
+let lamYar = +lambarYar;
+let lamWay = +lambarWayn;
 
+if(lamYar && lamWay){
 // Use Math.random() and the user's number to generate a random number
-let lambarNasiibah = Math.floor( Math.random()*sheegLambar+1);
+let lambarNasiibah = Math.floor( Math.random()*(lamWay-lamYar+1))
++lamYar;
 
 // Create a message displaying the random number
-const sooSaar = document.querySelector('main').innerHTML = (`${lambarNasiibah} waa lambar nasiib ah oo u dhaxeeya 1 iyo ${sheegLambar}`);
-
-
+const sooSaar = document.querySelector('main').innerHTML = (`${lambarNasiibah} waa lambar nasiib ah oo u dhaxeeya ${lamYar} iyo ${lamWay}`);
+} 
+else {const hadiiKale = document.querySelector('main').innerHTML = (`Walaal Gali Laba Lambar !!`); 
+}
